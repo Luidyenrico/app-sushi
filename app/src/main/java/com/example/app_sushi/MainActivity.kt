43f8f40d18f0a05257ff7f41fb12804480ea1f46
LuidyEnrico <80763934+Luidyenrico@users.noreply.github.com>
2023-11-27 08:55:17 -0300

    att

--- app/src/main/java/com/example/app_sushi/MainActivity.kt
+++ app/src/main/java/com/example/app_sushi/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.app_sushi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.tutorialwing.chipgroup.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupChipGroup()
     }
+
+    private fun setupChipGroup() {
+        binding.tudo.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 1st Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        binding.popular.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 2nd Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
+        binding.sushis.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 3rd Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
+        binding.barcas.setOnCloseIconClickListener {
+            binding.chipGroup.removeView(it)
+            Toast
+                .makeText(this, "Removed 4th Chip", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
 }
